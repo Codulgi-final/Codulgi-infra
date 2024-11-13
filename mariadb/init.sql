2a28428cac9e9a7188cae7e6bc2a0ca9316b056f
lsiwh37249 <lsiwh37249@gmail.com>
2024-11-13 17:56:54 +0900

    spring이랑 연동

--- mariadb/init.sql
+++ mariadb/init.sql
@@ -1,8 +1,12 @@
--- init.sql 예시
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL
-);
+-- 데이터베이스 생성
+CREATE DATABASE IF NOT EXISTS codulgi_db;
 
-INSERT INTO users (username, email) VALUES ('admin', 'admin@example.com');
+-- 사용자 생성 및 비밀번호 설정
+CREATE USER IF NOT EXISTS 'codulgi'@'%' IDENTIFIED BY 'codul1234';
+
+-- 해당 사용자에게 데이터베이스 권한 부여
+GRANT ALL PRIVILEGES ON codulgi_db.* TO 'codulgi'@'%';
+
+-- 권한 적용
+FLUSH PRIVILEGES;
+
